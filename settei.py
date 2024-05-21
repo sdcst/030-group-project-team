128aa9668158b9493fc66a924e7da4f620ae9059
FlaanKatsu <kole3600@gmail.com>
2024-05-21 13:35:42 -0700

    add settei entries for halflife, GDP, compound interest, simple interest. also, fix inherently broken halflife module

--- settei.py
+++ settei.py
@@ -7,6 +7,10 @@ import volume
 import flaan_modules
 import perimeter
 import tax
+import compoundinterest
+import halflife
+import simpleinterest
+import GDP
 
 def title():
     print("\n              _                            _        \n__      _____| | ___ ___  _ __ ___   ___  | |_ ___  \n\\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\ \n \\ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | \n  \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/ \n                                                    \n           _            _       _             \n  ___ __ _| | ___ _   _| | __ _| |_ ___  _ __ \n / __/ _` | |/ __| | | | |/ _` | __/ _ \\| '__|\n| (_| (_| | | (__| |_| | | (_| | || (_) | |   \n \\___\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \n                                              ")
@@ -27,7 +31,7 @@ def userDs():
 def selection():
     done = 0
     while done == 0:
-        print("This calculator has the following functionalities:\n[0] help\n[1] Find the area of a square\n[2] find surface area of cube or sphere\n[3] find volume of a cube\n[4] find if a triangle is acute, right, or obtuse using it's sides\n[5] find the perimeter of a rectangle, triangle, or circle\n[6] find the Δv of a rocket in a vacuum\n[7] tax calculator for BC, Canada\n[e] exit")
+        print("This calculator has the following functionalities:\n[0] help\n[1] Find the area of a square\n[2] find surface area of cube or sphere\n[3] find volume of a cube\n[4] find if a triangle is acute, right, or obtuse using it's sides\n[5] find the perimeter of a rectangle, triangle, or circle\n[6] find the Δv of a rocket in a vacuum\n[7] tax calculator for BC, Canada\n[8] Compound Interest Calculator\n[9] Halflife (chemestry) Calculator\n[10] Simple Interest Calculator\n[11] GDP calculator\n[e] exit")
         uw = str(input("To select an option, enter the number associated with the option: "))
         print("\n"*100)
         if uw == "0":
@@ -53,6 +57,15 @@ def selection():
             done = 1
         elif uw == "7":
             tax.tax()
+            done = 1
+        elif uw == "8":
+            compoundinterest.calculate_compound_interest()
+            done = 1
+        elif uw == "9":
+            halflife.hldo()
+            done = 1
+        elif uw == "10":
+            simpleinterest.simple_interest()
             done = 1
         elif uw == "e":
             print("oki doki, bye bye")
