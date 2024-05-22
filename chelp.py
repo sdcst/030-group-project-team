d2a911a1ec95e9815418891d1d440aee73436618
フランカツ!! <105261832+FlaanKatsu@users.noreply.github.com>
2024-05-22 12:32:44 -0700

    please add GDP to chelp

--- chelp.py
+++ chelp.py
@@ -1,2 +1,8 @@
 def chelp():
-    str(input("\nwhat do you need help with?"))
+    h = str(input("\nwhatxsf4*````` do you need help with?\n[6] for the rocket Δv module\n[9] for half life calculator\n[11] GDP calculator"))
+    if h == "11":
+        GDPhelp
+
+def GDPhelp():
+    print("please enter how to use GDP here")
+
