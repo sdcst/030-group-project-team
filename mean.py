6755c518a4d85fa082f8458a99afd4aabcb11510
creeeeso <crystalf7319@gmail.com>
2024-05-22 12:29:25 -0700

    mean

---
+++ mean.py
@@ -0,0 +1,13 @@
+def mean():
+    done = 0
+    while done == 0:
+        x = input("Enter numbers separated by spaces: ")
+        try:
+            e
+        except ValueError:
+            print("Please enter valid numbers.")
+        return
+
+
+if __name__ == "__main__":
+    mean()
