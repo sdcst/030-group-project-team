7b1fc3f708a625bdc4b52c97663e982279bc25a3
victoria1218 <zhangshuyi682@gmail.com>
2024-05-23 12:29:21 -0700

    mm

--- compoundinterest.py
+++ compoundinterest.py
@@ -1,9 +1,10 @@
 def calculate_compound_interest():
+
     principal = float(input("Enter the principal amount: "))
-    rate = float(input("Enter the interest rate (in percentage): "))
+    rate = float(input("Enter the interest rate (in percentage but donnot type %): "))
     time = float(input("Enter the time period (in years): "))
     tc = int(input("Enter the number of times interest is compounded per year: "))
-    a = principal * (1 + rate / (100 * tc)) ** (tc * time)
+    a = principal * (1 + (rate / 100) * tc) ** (tc * time)
     a = round(a,2)
     ci = a - principal
     ci = round (ci,2)
--- simpleinterest.py
+++ simpleinterest.py
@@ -1,4 +1,5 @@
 def simple_interest():
+
     principal = float(input("Enter the principal amount: "))
     rate = float(input("Enter the interest rate: "))
     time = float(input("Enter the time period (in years): "))
@@ -6,6 +7,7 @@ def simple_interest():
     i = round(i,2)
     ta= (principal + i)
     print(f"The simple interest is: {i}. The total amount is{ta}")
+    
 def main():
     simple_interest()
 if __name__ == "__main__":
